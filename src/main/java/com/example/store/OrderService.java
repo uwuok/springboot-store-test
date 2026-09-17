0358192ecfd2a4315299ab18387a97fe898dc046
uwuok <natsumi7129453@gmail.com>
2026-09-17 15:22:57 +0800

    3-4 setter injection

--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -8,14 +8,19 @@ public class OrderService {
 
     private PaymentService paymentService;
 
+
     // 透過 constructor 進行 dependency injection
-    public OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder() {
 //        var paymentService = new StripePaymentService();
         paymentService.processPayment(10);
 
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -9,7 +9,13 @@ public class StoreApplication {
     public static void main(String[] args) {
 
 //        SpringApplication.run(StoreApplication.class, args);
-        var orderService = new OrderService(new PayPalPaymentService());
+        var orderService = new OrderService();
+
+        // 這種實現方法如果忘記傳入參數會有 NULL pointer exception
+        // 在這種情況下 Payment service 是必須依賴項，而非可選的
+        // 所以在這種情況下使用 setter inject 是沒有意義的
+        // 再者，多數情況下還是用 constructor injection
+        orderService.setPaymentService(new PayPalPaymentService());
         orderService.placeOrder();
     }
 
